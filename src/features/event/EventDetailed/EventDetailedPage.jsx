2075b5e0ea878fe0fd7c44da07d3eded387737ac
Archer Hobson <supremeoverlord@Archers-MacBook-Pro.local>
2018-07-03 17:05:34 -0500

    set up event detail page routing and static styling

--- src/features/event/EventDetailed/EventDetailedPage.jsx
+++ src/features/event/EventDetailed/EventDetailedPage.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { Grid } from 'semantic-ui-react';
+import EventDetailedHeader from './EventDetailedHeader';
+import EventDetailedInfo from './EventDetailedInfo';
+import EventDetailedChat from './EventDetailedChat';
+import EventDetailedSidebar from './EventDetailedSidebar';
 
 const EventDetailedPage = () => {
+  console.log(EventDetailedPage)
   return (
-    <div>
-      <h1>Event Detailed Page</h1>
-    </div>
-  )
-}
+    <Grid>
+      <Grid.Column width={10}>
+        <EventDetailedHeader />
+        <EventDetailedInfo />
+        <EventDetailedChat />
+      </Grid.Column>
+      <Grid.Column width={5}>
+        <EventDetailedSidebar />
+      </Grid.Column>
+    </Grid>
+  );
+};
 
-export default EventDetailedPage
+export default EventDetailedPage;
