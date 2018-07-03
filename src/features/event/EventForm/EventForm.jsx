a12465194d4e9482305a5cf7c82c0c686953172c
Archer Hobson <supremeoverlord@Archers-MacBook-Pro.local>
2018-07-03 18:30:38 -0500

    set up event form with redux to display on header create event select and event detail manage event select

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -1,32 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Segment, Form, Button } from 'semantic-ui-react';
 
-const emptyEvent = {
-  title:'',
-  date:'',
-  city:'',
-  venue:'',
-  hostedBy:''
+//take state and ownprops since need to get state from root
+const mapState = (state, ownProps) => {
+  const eventId = ownProps.match.params.id;
+
+  let event = {
+    title:'',
+    date:'',
+    city:'',
+    venue:'',
+    hostedBy:''
+  }
+
+  if (eventId && state.events.length > 0) {
+    event = state.events.filter(event => event.id === eventId)[0];
+  }
+
+  return {
+    event
+  }
 }
 class EventForm extends Component {
   state = {
-    event: emptyEvent
-  }
-
-  componentDidMount() {
-    if (this.props.selectedEvent !== null) {
-      this.setState({
-        event: this.props.selectedEvent
-      })
-    }
-  }
-  //can get acces to next an previous props.
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.selectedEvent !== this.props.selectedEvent) {
-      this.setState({
-        event: nextProps.selectedEvent || emptyEvent
-      })
-    }
+    event: Object.assign({}, this.props.event)
   }
 
   onFormSubmit = (evt) => {
@@ -85,4 +83,4 @@ class EventForm extends Component {
   }
 }
 
-export default EventForm;
+export default connect(mapState)(EventForm);
