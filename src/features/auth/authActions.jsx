47e67502df8fc1f99d4fc07033ecfd75ba51b715
Archer Hobson <supremeoverlord@Archers-MacBook-Pro.local>
2018-07-06 11:17:04 -0500

    set user UID properties we want stored

--- src/features/auth/authActions.jsx
+++ src/features/auth/authActions.jsx
@@ -47,15 +47,22 @@ async (dispatch, getState, {getFirebase, getFirestore}) => {
 }
 
 export const socialLogin = (selectedProvider) => 
-    async (dispatch, getState, {getFirebase}) => {
+    async (dispatch, getState, {getFirebase, getFirestore}) => {
         const firebase = getFirebase();
+        const firestore = getFirestore();
         try {
             dispatch(closeModal());
             let user = await firebase.login({
                 provider: selectedProvider,
                 type: 'popup'
             })
-            console.log(user);
+            if (user.additionalUserInfo.isNewUser) {
+                await firestore.set(`users/${user.user.uid}`, {
+                    displayName: user.profile.displayName,
+                    photoURL: user.profile.avatarUrl,
+                    createdAt: firestore.FieldValue.serverTimestamp()
+                })
+            }
         } catch (error) {
             console.log(error);
         }
