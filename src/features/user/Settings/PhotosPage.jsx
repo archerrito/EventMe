44cdba72dcdd13a5faa7257570038422c6550d40
Archer Hobson <supremeoverlord@Archers-MacBook-Pro.local>
2018-07-11 18:26:42 -0500

    set up delete photo

--- src/features/user/Settings/PhotosPage.jsx
+++ src/features/user/Settings/PhotosPage.jsx
@@ -1,15 +1,34 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
 import {Image, Segment, Header, Divider, Grid, Button, Card, Icon} from 'semantic-ui-react';
 import Dropzone from 'react-dropzone';
 import Cropper from 'react-cropper';
 import 'cropperjs/dist/cropper.css';
 import { toastr } from 'react-redux-toastr';
-import { uploadProfileImage } from '../userActions';
+import { uploadProfileImage, deletePhoto } from '../userActions';
+
+const query = ({auth}) => {
+  return [
+    { collection: 'users',
+      doc: auth.uid,
+      subcollections: [{collection: 'photos'}],
+      storeAs: 'photos'
+}
+  ]
+}
 
 const actions = {
-  uploadProfileImage
+  uploadProfileImage,
+  deletePhoto
 }
+
+const mapState = (state) => ({
+  auth: state.firebase.auth,
+  profile: state.firebase.profile,
+  photos: state.firestore.ordered.photos
+})
 class PhotosPage extends Component {
   state = {
     files: [],
@@ -33,7 +52,14 @@ class PhotosPage extends Component {
       toastr.success('Success!', 'Photo has been uploaded')
     } catch (error) {
       toastr.error('Oops', error.message);
-      
+    }
+  }
+
+  handlePhotoDelete = (photo) => () => {
+    try {
+      this.props.deletePhoto(photo);
+    } catch (error) {
+      toastr.error('Oops', error.message)
     }
   }
 
@@ -58,6 +84,13 @@ class PhotosPage extends Component {
     }, 'image/jpeg')
   }
     render() {
+      const {photos, profile}= this.props;
+      let filteredPhotos;
+      if( photos) {
+        filteredPhotos = photos.filter(photo => {
+          return photo.url !== profile.photoURL
+        })
+      }
         return (
             <Segment>
                 <Header dividing size='large' content='Your Photos' />
@@ -112,23 +145,28 @@ class PhotosPage extends Component {
 
                 <Card.Group itemsPerRow={5}>
                     <Card>
-                        <Image src='https://randomuser.me/api/portraits/men/20.jpg'/>
+                        <Image src={profile.photoURL}/>
                         <Button positive>Main Photo</Button>
                     </Card>
-
-                        <Card >
-                            <Image
-                                src='https://randomuser.me/api/portraits/men/20.jpg'
-                            />
-                            <div className='ui two buttons'>
-                                <Button basic color='green'>Main</Button>
-                                <Button basic icon='trash' color='red' />
-                            </div>
-                        </Card>
+                    {photos && filteredPhotos.map((photo) => 
+                      <Card key={photo.id}>
+                      <Image
+                          src={photo.url}
+                      />
+                      <div className='ui two buttons'>
+                          <Button basic color='green'>Main</Button>
+                          <Button onClick={this.handlePhotoDelete(photo)} basic icon='trash' color='red' />
+                      </div>
+                      </Card>
+                    
+                    )}
                 </Card.Group>
             </Segment>
         );
     }
 }
 
-export default connect(null, actions)(PhotosPage);
+export default compose(
+  connect(mapState, actions),
+  firestoreConnect(auth => query(auth))
+)(PhotosPage);
--- src/features/user/userActions.jsx
+++ src/features/user/userActions.jsx
@@ -1,5 +1,7 @@
 import moment from 'moment';
+import cuid from 'cuid';
 import { toastr } from 'react-redux-toastr'
+import { firebaseStateReducer } from '../../../node_modules/react-redux-firebase';
 
 export const updateProfile = (user) =>
   async (dispatch, getState, {getFirebase}) => {
@@ -19,12 +21,13 @@ export const updateProfile = (user) =>
 
   export const uploadProfileImage =(file, fileName) =>
   async(dispatch, getState, {getFirebase, getFirestore}) => {
-      const firebase = getFirebase();
+        const imageName = cuid();  
+        const firebase = getFirebase();
       const firestore = getFirestore();
       const user = firebase.auth().currentUser;
       const path = `${user.uid}/user_images`;
       const options = {
-          name: fileName
+          name: imageName
       };
       try {
             //upload file to firebase storage
@@ -53,11 +56,29 @@ export const updateProfile = (user) =>
                 subcollections: [{collection: 'photos'}]
             }, {
                 //only fields photo will have are name and url
-                name: fileName,
+                name: imageName,
                 url: downloadURL
             })
       } catch (error) {
           console.log(error);
           throw new Error('Problem uploading photo');
       }
+  }
+
+  export const deletePhoto = (photo) =>
+  async (dispatch, getState, {getFirebase, getFirestore}) => {
+    const firebase = getFirebase();
+    const firestore = getFirestore();
+    const user = firebase.auth().currentUser;
+    try {
+        await firebase.deleteFile(`${user.uid}/user_images/${photo.name}`);
+        await firestore.delete({
+            collection: 'users',
+            doc: user.uid,
+            subcollections: [{collection: 'photos', doc: photo.id}]
+        })
+    } catch (error) {
+        console.log(error);
+        throw new Error('Problem deleting the photo')
+    }
   }
