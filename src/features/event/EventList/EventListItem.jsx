402f5c16b38fe4d089d72238a282e6963292152a
Archer Hobson <supremeoverlord@Archers-MacBook-Pro.local>
2018-07-12 13:41:18 -0500

    set up links for events id's and user id's

--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -3,6 +3,7 @@ import { Segment, Item, Icon, List, Button, Label } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import format from 'date-fns/format';
 import EventListAttendee from './EventListAttendee';
+import { objectToArray } from '../../../app/common/util/helpers'
 
 class EventListItem extends Component {
   render() {
@@ -17,9 +18,9 @@ class EventListItem extends Component {
                       circular 
                       src={event.hostPhotoURL}/>
                     <Item.Content>
-                      <Item.Header as="a">{event.title}</Item.Header>
+                      <Item.Header as={Link} to={`/event/${event.id}`}>{event.title}</Item.Header>
                       <Item.Description>
-                        Hosted by <a>{event.hostedBy}</a>
+                      Hosted by <Link to={`/profile/${event.hostUid}`}>{event.hostedBy}</Link>
                       </Item.Description>
                       {event.cancelled && 
                       <Label style={{top: '-40px'}} ribbon='right' color='red' content='This event has been cancelled' />
@@ -30,14 +31,15 @@ class EventListItem extends Component {
               </Segment>
               <Segment>
                 <span>
-                  <Icon name="clock" /> {format(event.date.toDate(), 'dddd Do MMMM')} at{' '} {format(event.date.toDate(),'HH:mm')} |
+                  <Icon name="clock" /> {format(event.date.toDate(), 'dddd Do MMMM')} at{' '} 
+                  {format(event.date.toDate(),'HH:mm')} |
                   <Icon name="marker" /> {event.venue}
                 </span>
               </Segment>
               <Segment secondary>
                 <List horizontal>
-                {event.attendees && Object.values(event.attendees).map((attendee, index) => (
-                  <EventListAttendee key={index} attendee={attendee}/>
+                {event.attendees && objectToArray(event.attendees).map((attendee) => (
+                  <EventListAttendee key={attendee.id} attendee={attendee}/>
                 ))}
                 </List>
               </Segment>
