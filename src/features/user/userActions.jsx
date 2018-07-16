8c681ef6b893ae5e5723dc049ddfec300bb3a738
Archer Hobson <supremeoverlord@Archers-MacBook-Pro.local>
2018-07-15 22:01:41 -0500

    use settings display name instead of auth display name

--- src/features/user/userActions.jsx
+++ src/features/user/userActions.jsx
@@ -144,12 +144,12 @@ export const updateProfile = (user) =>
             const firestore = firebase.firestore();
             const user = firebase.auth().currentUser;
             //want to get from firebase profile, 
-            const photoURL = getState().firebase.profile.photoURL;
+            const profile = getState().firebase.profile;
             const attendee = {
                 going: true,
                 joinDate: Date.now(),
-                photoURL: photoURL|| '/assets/user.png',
-                displayName: user.displayName,
+                photoURL: profile.photoURL|| '/assets/user.png',
+                displayName: profile.displayName,
                 host: false
             }
             try {
